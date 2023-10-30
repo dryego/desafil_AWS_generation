96c4e7dc2500f856549db8ad8dfabcb73d1af798
dryego <dryego.lisboa@gmail.com>
2023-10-30 14:03:23 -0300

    deploy

--- src/connections/knex.js
+++ src/connections/knex.js
@@ -1,22 +1,25 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// const knex = require("knex")({
-//   client: "pg",
-//   connection: {
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORTA,
-//   },
-// });
-
 const knex = require("knex")({
   client: "pg",
-  connection: process.env.DATABASE_URL,
-  searchPath: ["knex", "public"],
+  connection: {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORTA,
+  },
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
 });
+
+// const knex = require("knex")({
+//   client: "pg",
+//   connection: process.env.DATABASE_URL,
+//   searchPath: ["knex", "public"],
+//   ssl: {
+//     rejectUnauthorized: true,
+//   },
+// });
 
 module.exports = knex;
