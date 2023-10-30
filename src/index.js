1b44e45c71a2e0a816cb838d19647428981d4331
dryego <dryego.lisboa@gmail.com>
2023-10-30 16:03:23 -0300

    desploy

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.get("/", (res, req) => res.json({ Menssagem: "tudo OK!" }));
-
 app.use("/aluno", rotasAluno);
 
 app.listen(process.env.PORT, console.log("Servidor Inicializado..."));
